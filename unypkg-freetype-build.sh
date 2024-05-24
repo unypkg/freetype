8dd9370d3733397384a8e2b2f3423ec91ce717be
michacassola <mich@casso.la>
2024-05-24 21:59:30 +0200

    Update unypkg-freetype-build.sh

--- unypkg-freetype-build.sh
+++ unypkg-freetype-build.sh
@@ -11,7 +11,7 @@ set -vx
 wget -qO- uny.nu/pkg | bash -s buildsys
 
 ### Installing build dependencies
-#unyp install python expat openssl
+unyp install libpng
 
 #pip3_bin=(/uny/pkg/python/*/bin/pip3)
 #"${pip3_bin[0]}" install --upgrade pip
@@ -35,13 +35,13 @@ mkdir -pv /uny/sources
 cd /uny/sources || exit
 
 pkgname="freetype"
-pkggit="https://github.com/freetype/freetype.git refs/tags/*"
+pkggit="https://gitlab.freedesktop.org/freetype/freetype.git refs/tags/VER-*"
 gitdepth="--depth=1"
 
 ### Get version info from git remote
 # shellcheck disable=SC2086
-latest_head="$(git ls-remote --refs --tags --sort="v:refname" $pkggit | grep -E "v[0-9.]+$" | tail --lines=1)"
-latest_ver="$(echo "$latest_head" | grep -o "v[0-9.].*" | sed "s|v||")"
+latest_head="$(git ls-remote --refs --tags --sort="v:refname" $pkggit | grep -E "VER-[0-9-]+$" | tail --lines=1)"
+latest_ver="$(echo "$latest_head" | grep -o "VER-[0-9-].*" | sed -e "s|VER-||" -e "s|-|.|g")"
 latest_commit_id="$(echo "$latest_head" | cut --fields=1)"
 
 version_details
@@ -77,11 +77,14 @@ get_include_paths
 
 unset LD_RUN_PATH
 
-./configure \
-    --prefix=/uny/pkg/"$pkgname"/"$pkgver"
+sh autogen.sh
+
+./configure --prefix=/uny/pkg/"$pkgname"/"$pkgver" \
+    --enable-freetype-config \
+    --disable-static
 
 make -j"$(nproc)"
-make -j"$(nproc)" check 
+
 make -j"$(nproc)" install
 
 ####################################################
